2a57aa9bbe866be5562e121511c049b768c1f0af
geogeekSenior <alejo_gnr15@hotmail.com>
2019-03-05 09:00:34 -0500

    paginacion completa, posts funcionando y cuenta de palabras para postear

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,303 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <form v-on:submit="postear()">
+                  <div class="form-group">
+                    <label for="nombre"><h3>Comparte tu Opinión</h3></label>
+                    <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu Nombre" title="Nombre" v-model="nombre" required>
+                    <small id="nombreHelp" class="form-text text-muted">Nombre visible para la comunidad.</small>
+                  </div>
+                  <div class="form-group">
+                      <label for="txtArea"><h4>Ingresa tu Comentario</h4></label>
+                      <textarea class="form-control" id="txtArea" rows="3" title="Comentarios" v-model="message" v-on:keydown="countdown()" required></textarea>
+                      <small id="comHelp" class="form-text text-muted" v-bind:class="{'text-danger': hasError }">{{remainingCount}} |:V*| Escribe tu opinión en menos de {{maxCount}} carácteres.</small>
+                  </div>
+                  <div class="form-group" style= "visibility: hidden; color:red" id="textAdvertencia">
+                      <label>Excediste el número de caracteres</label>                      
+                  </div>
+                  <button type="submit" v-on:click="" class="btn btn-primary" id="botonSubmit">Comentar</button>
+                </form>
+          </div>
+      </div>
+      <!-- <div class="row justify-content-center">
+        <div class="col-md-8">
+          <h2 class="page-header">Comentarios</h2>
+            <section class="comment-list" v-if="arregloPosts && arregloPosts.length" v-model="arregloPosts">
+              <article class="row" v-for="post of arregloPosts">
+                <div>
+                  <div class="col-md-2 col-sm-2 col-lg-2 hidden-xs">
+                    <figure class="thumbnail">
+                      <img class="img-responsive" src="http://www.tangoflooring.ca/wp-content/uploads/2015/07/user-avatar-placeholder.png" />
+                      <figcaption class="text-center">{{post.nombre}}</figcaption>
+                    </figure>
+                  </div>
+                  <div class="col-md-10 col-sm-10 col-lg-10">
+                    <div class="panel panel-default arrow left">
+                      <div class="panel-body">
+                        <header class="text-left">
+                          <div class="comment-user"><i class="fa fa-user"></i>{{post.nombre}}</div>
+                          <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i>{{post.fechaHora}}</time>
+                        </header>
+                        <div class="comment-post">
+                          <p>{{post.comentario}}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </section>
+        </div>
+      </div> -->
+      <div>
+        <pagination :list-data="arregloPosts"/>
+      </div>
+    </div>
 </template>
 
 <script>
+import pagination from './pagination.vue';
+import axios from 'axios';///importar la dependencia de axios
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    pagination
+  },
+  data(){
+    return{
+      arregloPosts: [],
+      maxCount: 300,
+      remainingCount: 300,
+      message: '',
+      hasError: false,
+      nombre: '',
+      //expresiones regulares
+      groserias: ["puta", "puto","marica","pirobo","gonorrea","hijo de puta","malparido"],
+      groseria: false
+    }
+  },
+  created() {
+      this.obtenerPosts();
+  },
+  methods:{
+    //////////////////
+    regularExpression: function(){
+      // [dominio] indica un dominio que puede ir en la cadena de texto
+      // /expresion/ indica la expresión regular sirviendo como un formato o verificando contenido en el texto
+      // new RegExp() es un metodo para comprobar la expresión regular
+      // /expresion/i al final de la expresion lo hace case insentitive sin la i es case sensitive 
+      // ? indica que la presencia de un elemento es opcional, el caracter o grupo anterior a este signo
+      // () para agrupar una parte del patron
+      // test evalua si alguna parte del texto contiene la expresion evaluada, si la cadena tiene alguna palabra 
+      // que coincida con el formato va a devolver true
+      // /^ expresion $/ se usan tipicamente al inicio y final del patron y van por dentro del delimitador
+      // buscan coincidencias al inicio y al final de la cadena
+      // \w indica el patron para usar [a-zA-z0-9_] una abrevación para esto
+      //  \d es equivalente al patrón [0-9] /^([VE]-)?\d{1,8}$/i
+      const cedula    = 'v-11122233'// --- cadena a probar
+      const ced = 'xxV-111111'
+      const regExp    = new RegExp( /[VE]-[0-9]{1,8}/) // --- sin comillas
+      const regExp2 = new RegExp(/([VE]-)?[0-9]{1,8}/i) // --- no es obligatorio el uso de VE con el guion
+      const regExp3 = new RegExp(/^([VE]-)?[0-9]{1,8}$/i) // se asegura que la cadena completa coincide
+
+      const resultado = regExp.test( cedula )
+      const resultado2 = regExp2.test(cedula)
+      const resultado3 = regExp3.test(ced)
+      //--
+      console.log ( resultado ) // --- true = coincide, false = no-coincide
+      console.log(resultado2)
+      console.log(resultado3)
+    },
+    //comprobar si los posts tienen una groseria con expresiones regulares
+    filtro: function(){
+      var text = this.message;
+      // console.log(text);
+      for(var i = 0; i < this.groserias.length;i++){
+          var regex = new RegExp("(^|\\s)"+this.groserias[i]+"($|(?=\\s))","gi");
+          // console.log(regex)
+          var evaluacion = regex.test(text);
+          if(evaluacion == true){
+            console.log("groseria");
+            this.groseria = true;
+          }          
+      }
+    },
+    obtenerPosts: function(){
+      console.log("obtener");
+      var inst = this;
+        // this.arreglo = [];
+        axios.get('https://cors-anywhere.herokuapp.com/geoapps.esri.co/TheService/api/mongo/?format=json')
+        // axios.get('data/prueba.json/') //data de prueba
+        .then(function(response){
+            var arreglo = response.data;
+            inst.arregloPosts = arreglo.reverse(); //para ver los posts mas recientes
+            console.log(inst.arregloPosts); // ex.: { user: 'Your User'}
+            console.log(response.status); // ex.: 200
+      });
+    },
+    //funcion para calcular cuantos caracteres tiene por escribir la persona
+    countdown: function() {
+      // console.log(this.message);
+      this.remainingCount = this.maxCount - this.message.length;
+      this.hasError = this.remainingCount < 0;
+      if(this.remainingCount<=0){
+        document.getElementById("textAdvertencia").style.visibility = "visible";
+        document.getElementById("botonSubmit").disabled = true;
+      }else{
+        document.getElementById("textAdvertencia").style.visibility = "hidden";
+        document.getElementById("botonSubmit").disabled = false;
+      }
+    },
+    obtenerPosts: function(){
+      var inst = this;
+        // this.arreglo = [];
+        axios.get('https://cors-anywhere.herokuapp.com/geoapps.esri.co/TheService/api/mongo/?format=json')
+        // axios.get('data/prueba.json/') //data de prueba
+        .then(function(response){
+            var arreglo = response.data;
+            inst.arregloPosts = arreglo.reverse(); //para ver los posts mas recientes
+            console.log(inst.arregloPosts); // ex.: { user: 'Your User'}
+            console.log(response.status); // ex.: 200
+      });
+    },
+
+    postear: function(){
+        try {
+          var inst = this;
+        if(inst.remainingCount >= 0){ ///comporbar si tiene mas de los 300 carateres escritos
+            var headers = {
+            "Content-Type": "application/json",
+            "Allow": "*",
+          };
+          // var usuario = document.getElementById("nombre").value;
+          // var comentario = document.getElementById("txtArea").value;
+          var fecha = new Date();
+          var f = fecha.getDate()+"/"+(fecha.getMonth()+1)+"/"+fecha.getFullYear();
+          var h = fecha.getHours()+":"+fecha.getMinutes();
+          var fechaCom = f+" "+h;
+          // var idd = inst.arregloPosts.length+1;
+
+          // console.log(inst.message);
+          // console.log(inst.nombre);
+          var data = {
+                    // id: idd,
+                    nombre: inst.nombre,
+                    comentario: inst.message,
+                    fechaHora: fechaCom
+                };
+          var test= JSON.stringify(data);
+          // console.log(test);
+          axios.post('https://cors-anywhere.herokuapp.com/geoapps.esri.co/TheService/api/mongo/',  test, {headers: headers})
+                // axios.post('geoapps.esri.co:81/api/reporte/',  test, {headers: headers})
+                .then(function (response){
+                    console.log("Reporte agregado");
+                    // alert("Reporte agregado");
+                })
+                .catch(function (error){
+                    console.log(error);
+                    console.log("No se pudo agregar");
+                });
+        }
+        else{
+          alert("Pasó el limite de caracteres permitidos");
+        }
+      } catch (error) {
+        console.log(error);
+        console.log("error al registrar");
+      }
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+/*font Awesome http://fontawesome.io*/
+@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+/*Comment List styles*/
+.comment-list .row {
+  margin-bottom: 0px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.comment-list .panel .panel-heading {
+  padding: 4px 15px;
+  position: absolute;
+  border:none;
+  /*Panel-heading border radius*/
+  border-top-right-radius:0px;
+  top: 1px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.comment-list .panel .panel-heading.right {
+  border-right-width: 0px;
+  /*Panel-heading border radius*/
+  border-top-left-radius:0px;
+  right: 16px;
 }
-a {
-  color: #42b983;
+.comment-list .panel .panel-heading .panel-body {
+  padding-top: 6px;
+}
+.comment-list figcaption {
+  /*For wrapping text in thumbnail*/
+  word-wrap: break-word;
+}
+/* Portrait tablets and medium desktops */
+@media (min-width: 768px) {
+  .comment-list .arrow:after, .comment-list .arrow:before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-color: transparent;
+  }
+  .comment-list .panel.arrow.left:after, .comment-list .panel.arrow.left:before {
+    border-left: 0;
+  }
+  /*****Left Arrow*****/
+  /*Outline effect style*/
+  .comment-list .panel.arrow.left:before {
+    left: 0px;
+    top: 30px;
+    /*Use boarder color of panel*/
+    border-right-color: inherit;
+    border-width: 16px;
+  }
+  /*Background color effect*/
+  .comment-list .panel.arrow.left:after {
+    left: 1px;
+    top: 31px;
+    /*Change for different outline color*/
+    border-right-color: #FFFFFF;
+    border-width: 15px;
+  }
+  /*****Right Arrow*****/
+  /*Outline effect style*/
+  .comment-list .panel.arrow.right:before {
+    right: -16px;
+    top: 30px;
+    /*Use boarder color of panel*/
+    border-left-color: inherit;
+    border-width: 16px;
+  }
+  /*Background color effect*/
+  .comment-list .panel.arrow.right:after {
+    right: -14px;
+    top: 31px;
+    /*Change for different outline color*/
+    border-left-color: #FFFFFF;
+    border-width: 15px;
+  }
+}
+.comment-list .comment-post {
+  margin-top: 6px;
+}
+
+textarea {
+  overflow: auto;
+  resize: none;
+}
+
+.text-danger {
+  color: red;
 }
 </style>
---
+++ src/components/pagination.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="container">
+      <div>
+        <!-- <ul>
+          <li v-for="p in paginatedData">
+            {{p.nombre}} 
+            {{p.fechaHora}}  
+            {{p.comentario}}
+          </li>
+        </ul> -->
+        <div class="row justify-content-center">
+        <div class="col-md-8">
+          <h2 class="page-header">Comentarios</h2>
+            <section class="comment-list">
+              <article class="row" v-for="post of paginatedData">
+                <div>
+                  <div class="col-md-2 col-sm-2 col-lg-2 hidden-xs">
+                    <figure class="thumbnail">
+                      <img class="img-responsive" src="http://www.tangoflooring.ca/wp-content/uploads/2015/07/user-avatar-placeholder.png" />
+                      <figcaption class="text-center">{{post.nombre}}</figcaption>
+                    </figure>
+                  </div>
+                  <div class="col-md-10 col-sm-10 col-lg-10">
+                    <div class="panel panel-default arrow left">
+                      <div class="panel-body">
+                        <header class="text-left">
+                          <div class="comment-user"><i class="fa fa-user"></i>{{post.nombre}}</div>
+                          <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i>{{post.fechaHora}}</time>
+                        </header>
+                        <div class="comment-post">
+                          <p>{{post.comentario}}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </section>
+            </div>
+        </div>
+        <button class="btn btn-outline-success" :disabled="pageNumber === 0" @click="prevPage">
+          << 10 Ant.
+        </button>
+        <button class="btn btn-outline-success" :disabled="pageNumber >= pageCount -1" @click="nextPage">
+          10 Sig. >>
+        </button>
+      </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';///importar la dependencia de axios
+export default {
+  name: 'pagination',
+  data(){
+    return{
+      //paginacion
+      pageNumber: 0,  // default to page 0,
+    }
+  },
+  created(){
+  },
+  ///propiedades para crear la paginacion
+  props:{
+    listData:{
+      type:Array,
+      required:true
+    },
+    size:{
+      type:Number,
+      required:false,
+      default: 10
+    }
+  },
+  ///para paginacion de posts
+  computed:{
+    pageCount: function(){
+      let l = this.listData.length,
+          s = this.size;
+      return Math.ceil(l/s);
+    },
+    paginatedData: function(){
+    const start = this.pageNumber * this.size,
+          end = start + this.size;
+     return this.listData.slice(start, end);
+    } 
+  },
+  methods:{
+    ///para la paginacion
+    nextPage: function(){
+        this.pageNumber++;
+    },
+    prevPage: function(){
+      this.pageNumber--;
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*font Awesome http://fontawesome.io*/
+@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+/*Comment List styles*/
+.comment-list .row {
+  margin-bottom: 0px;
+}
+.comment-list .panel .panel-heading {
+  padding: 4px 15px;
+  position: absolute;
+  border:none;
+  /*Panel-heading border radius*/
+  border-top-right-radius:0px;
+  top: 1px;
+}
+.comment-list .panel .panel-heading.right {
+  border-right-width: 0px;
+  /*Panel-heading border radius*/
+  border-top-left-radius:0px;
+  right: 16px;
+}
+.comment-list .panel .panel-heading .panel-body {
+  padding-top: 6px;
+}
+.comment-list figcaption {
+  /*For wrapping text in thumbnail*/
+  word-wrap: break-word;
+}
+/* Portrait tablets and medium desktops */
+@media (min-width: 768px) {
+  .comment-list .arrow:after, .comment-list .arrow:before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-color: transparent;
+  }
+  .comment-list .panel.arrow.left:after, .comment-list .panel.arrow.left:before {
+    border-left: 0;
+  }
+  /*****Left Arrow*****/
+  /*Outline effect style*/
+  .comment-list .panel.arrow.left:before {
+    left: 0px;
+    top: 30px;
+    /*Use boarder color of panel*/
+    border-right-color: inherit;
+    border-width: 16px;
+  }
+  /*Background color effect*/
+  .comment-list .panel.arrow.left:after {
+    left: 1px;
+    top: 31px;
+    /*Change for different outline color*/
+    border-right-color: #FFFFFF;
+    border-width: 15px;
+  }
+  /*****Right Arrow*****/
+  /*Outline effect style*/
+  .comment-list .panel.arrow.right:before {
+    right: -16px;
+    top: 30px;
+    /*Use boarder color of panel*/
+    border-left-color: inherit;
+    border-width: 16px;
+  }
+  /*Background color effect*/
+  .comment-list .panel.arrow.right:after {
+    right: -14px;
+    top: 31px;
+    /*Change for different outline color*/
+    border-left-color: #FFFFFF;
+    border-width: 15px;
+  }
+}
+.comment-list .comment-post {
+  margin-top: 6px;
+}
+
+textarea {
+  overflow: auto;
+  resize: none;
+}
+
+.text-danger {
+  color: red;
+}
+
+/* // para la paginacion  */
+
+ul{
+  padding: 4px 4px;
+  border: 1px solid black;
+  
+}
+li{
+  list-style-type:none;
+  padding:4px 4px;
+}
+li:hover{
+  background-color:#eee;
+}
+li:nth-child(2n){
+  background-color:#ddd;
+}
+li:nth-child(2n):hover{
+  background-color:#ccc;
+}
+
+button{
+  width:100px;
+  height:40px;
+  background-color:#eef;
+}
+
+button:hover{
+  cursor:pointer;
+}
+button:hover:disabled{
+  cursor:not-allowed;
+}
+</style>
